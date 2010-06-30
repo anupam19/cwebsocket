25f11163241692f3ec3f5226784a43345c353576
to.merge <to.merge@6d64d8ca-fcb9-0475-b6d9-75f76008fbb2>
2010-06-30 18:05:23 +0000

    first commit

---
+++ websocket.h
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2010 Putilov Andrey
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+#ifndef WEBSOCKET_H
+#define	WEBSOCKET_H
+
+enum ws_frame_type {
+	WS_ERROR_FRAME,
+	WS_INCOMPLETE_FRAME,
+	WS_TEXT_FRAME,
+	WS_BINARY_FRAME,
+	WS_OPENING_FRAME,
+	WS_CLOSING_FRAME
+};
+
+struct handshake {
+	char *resource;
+	char *host;
+	char *origin;
+	char *protocol;
+	char *key1;
+	char *key2;
+	char key3[8];
+};
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+enum ws_frame_type ws_parse_handshake(const char *input_frame, size_t input_len,
+	struct handshake *hs);
+
+enum ws_frame_type ws_get_handshake_answer(const struct handshake *hs,
+	char *out_frame, size_t *out_len);
+
+enum ws_frame_type ws_make_frame(const char *data, size_t data_len,
+	char *out_frame, size_t *out_len);
+
+enum ws_frame_type ws_parse_input_frame(const char *input_frame, size_t input_len,
+	char *out_data, size_t *out_len);
+
+void nullhandshake(struct handshake *hs);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* WEBSOCKET_H */
+
